85b29adfb40c0af60a6da0ea40b34ecc01d2f43b
moulinier <vmoulinier33@gmail.com>
2019-01-31 15:22:56 +0000

    teamJbzz

--- main/user_main.c
+++ main/user_main.c
@@ -27,169 +27,190 @@
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
-#define EXAMPLE_ESP_WIFI_MODE_AP   CONFIG_ESP_WIFI_MODE_AP //TRUE:AP FALSE:STA
-#define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
-#define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
-#define EXAMPLE_MAX_STA_CONN       CONFIG_MAX_STA_CONN
+#define EXAMPLE_ESP_WIFI_MODE_AP   TRUE //TRUE:AP FALSE:STA
+#define EXAMPLE_ESP_WIFI_SSID      "vagrant"
+#define EXAMPLE_ESP_WIFI_PASS      ""
+#define EXAMPLE_MAX_STA_CONN       1
 
 #define GREEN_LED_PIN GPIO_Pin_14
 #define GREEN_LED_NUM GPIO_NUM_14
 
+#define RED_LED_PIN GPIO_Pin_12
+#define RED_LED_NUM GPIO_NUM_12
+
 #define TILT_SENSOR_PIN GPIO_Pin_5
 #define TILT_SENSOR_NUM GPIO_NUM_5
 
+#define BUTTON_PIN GPIO_Pin_4
+#define BUTTON_NUM GPIO_NUM_4
+
 TimerHandle_t timer_led;
+TimerHandle_t timer_button;
 
-// /* FreeRTOS event group to signal when we are connected*/
-// static EventGroupHandle_t wifi_event_group;
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t wifi_event_group;
 
-// /* The event group allows multiple bits for each event,
-//    but we only care about one event - are we connected
-//    to the AP with an IP? */
-// const int WIFI_CONNECTED_BIT = BIT0;
+/* The event group allows multiple bits for each event,
+   but we only care about one event - are we connected
+   to the AP with an IP? */
+const int WIFI_CONNECTED_BIT = BIT0;
 
-// static const char *TAG = "simple wifi";
+static const char *TAG = "simple wifi";
 
-// static esp_err_t event_handler(void *ctx, system_event_t *event) {
-//     switch(event->event_id) {
-//     case SYSTEM_EVENT_STA_START:
-//         esp_wifi_connect();
-//         break;
-//     case SYSTEM_EVENT_STA_GOT_IP:
-//         ESP_LOGI(TAG, "got ip:%s",
-//                  ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
-//         xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
-//         break;
-//     case SYSTEM_EVENT_AP_STACONNECTED:
-//         ESP_LOGI(TAG, "station:"MACSTR" join, AID=%d",
-//                  MAC2STR(event->event_info.sta_connected.mac),
-//                  event->event_info.sta_connected.aid);
-//         break;
-//     case SYSTEM_EVENT_AP_STADISCONNECTED:
-//         ESP_LOGI(TAG, "station:"MACSTR"leave, AID=%d",
-//                  MAC2STR(event->event_info.sta_disconnected.mac),
-//                  event->event_info.sta_disconnected.aid);
-//         break;
-//     case SYSTEM_EVENT_STA_DISCONNECTED:
-//         esp_wifi_connect();
-//         xEventGroupClearBits(wifi_event_group, WIFI_CONNECTED_BIT);
-//         break;
-//     default:
-//         break;
-//     }
-//     return ESP_OK;
-// }
+static esp_err_t event_handler(void *ctx, system_event_t *event) {
+    switch(event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+        esp_wifi_connect();
+        break;
+    case SYSTEM_EVENT_STA_GOT_IP:
+        ESP_LOGI(TAG, "got ip:%s",
+                 ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
+        xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
+        break;
+    case SYSTEM_EVENT_AP_STACONNECTED:
+        ESP_LOGI(TAG, "station:"MACSTR" join, AID=%d",
+                 MAC2STR(event->event_info.sta_connected.mac),
+                 event->event_info.sta_connected.aid);
+        break;
+    case SYSTEM_EVENT_AP_STADISCONNECTED:
+        ESP_LOGI(TAG, "station:"MACSTR"leave, AID=%d",
+                 MAC2STR(event->event_info.sta_disconnected.mac),
+                 event->event_info.sta_disconnected.aid);
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        esp_wifi_connect();
+        xEventGroupClearBits(wifi_event_group, WIFI_CONNECTED_BIT);
+        break;
+    default:
+        break;
+    }
+    return ESP_OK;
+}
 
-// void wifi_init_softap() {
-//     wifi_event_group = xEventGroupCreate();
+void wifi_init_softap() {
+    wifi_event_group = xEventGroupCreate();
 
-//     tcpip_adapter_init();
-//     ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
 
-//     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-//     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-//     wifi_config_t wifi_config = {
-//         .ap = {
-//             .ssid = EXAMPLE_ESP_WIFI_SSID,
-//             .ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID),
-//             .password = EXAMPLE_ESP_WIFI_PASS,
-//             .max_connection = EXAMPLE_MAX_STA_CONN,
-//             .authmode = WIFI_AUTH_WPA_WPA2_PSK
-//         },
-//     };
-//     if (strlen(EXAMPLE_ESP_WIFI_PASS) == 0) {
-//         wifi_config.ap.authmode = WIFI_AUTH_OPEN;
-//     }
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID),
+            .password = EXAMPLE_ESP_WIFI_PASS,
+            .max_connection = EXAMPLE_MAX_STA_CONN,
+            .authmode = WIFI_AUTH_WPA_WPA2_PSK
+        },
+    };
+    if (strlen(EXAMPLE_ESP_WIFI_PASS) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
 
-//     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-//     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
-//     ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
 
-//     ESP_LOGI(TAG, "wifi_init_softap finished.SSID:%s password:%s",
-//              EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
-// }
+    ESP_LOGI(TAG, "wifi_init_softap finished.SSID:%s password:%s",
+             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+}
 
-// void wifi_init_sta() {
-//     wifi_event_group = xEventGroupCreate();
+void wifi_init_sta() {
+    wifi_event_group = xEventGroupCreate();
 
-//     tcpip_adapter_init();
-//     ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL) );
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL) );
 
-//     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-//     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-//     wifi_config_t wifi_config = {
-//         .sta = {
-//             .ssid = EXAMPLE_ESP_WIFI_SSID,
-//             .password = EXAMPLE_ESP_WIFI_PASS
-//         },
-//     };
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .password = EXAMPLE_ESP_WIFI_PASS
+        },
+    };
 
-//     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-//     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
-//     ESP_ERROR_CHECK(esp_wifi_start() );
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
 
-//     ESP_LOGI(TAG, "wifi_init_sta finished.");
-//     ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
-//              EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
-// }
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+    ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
+             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+}
 
 void timer_green_led(TimerHandle_t th) {
     gpio_set_level(GREEN_LED_NUM, 0);
 }
 
+void timer_buttons(TimerHandle_t th) {
+    gpio_set_level(RED_LED_NUM, 0);
+}
+
 void handle_tilt(void *param) {
     gpio_set_level(GREEN_LED_NUM, 1);
     xTimerStartFromISR(timer_led, NULL) ;
+}
 
-    //   int duration = pdMS_TO_TICKS(200) ;
-    //     vTaskDelay(duration) ;
-    //     
-    //     vTaskDelay(duration) ;
-    // for(int a=0;a<100;a++) {
-    //     vTaskDelay(duration) ;
-    //     gpio_set_level(GREEN_LED_NUM, 1);
-    //     vTaskDelay(duration) ;
-    //     gpio_set_level(GREEN_LED_NUM, 0);
-    // }
+void handle_button(void *param) {
+    gpio_set_level(RED_LED_NUM, 1);
+    xTimerStartFromISR(timer_button, NULL) ;
 }
 
 void app_main(void) {
-
+    gpio_install_isr_service(0) ;
     gpio_config_t c;
+
+    //LED VERTE
     c.mode = GPIO_MODE_OUTPUT;
     c.pin_bit_mask = GREEN_LED_PIN;
     c.intr_type = GPIO_INTR_DISABLE;
     c.pull_down_en = c.pull_up_en = 0;
     gpio_config(&c);
 
+    //LED ROUGE
+    c.mode = GPIO_MODE_OUTPUT;
+    c.pin_bit_mask = RED_LED_PIN;
+    c.intr_type = GPIO_INTR_DISABLE;
+    c.pull_down_en = c.pull_up_en = 0;
+    gpio_config(&c);
+
+    //TILT
     c.mode = GPIO_MODE_INPUT;
     c.pin_bit_mask = TILT_SENSOR_PIN;
     c.intr_type = GPIO_INTR_POSEDGE;
     c.pull_down_en = c.pull_up_en = 0;
     gpio_config(&c);
 
-    timer_led = xTimerCreate("Timer", pdMS_TO_TICKS(10), pdFALSE, 0, timer_green_led) ;
-
-    gpio_install_isr_service(0) ;
+    timer_led = xTimerCreate("TimerLed", pdMS_TO_TICKS(10), pdFALSE, 0, timer_green_led);
     gpio_isr_handler_add(TILT_SENSOR_NUM, handle_tilt, NULL);
-
     
+     //button
+    c.mode = GPIO_MODE_INPUT;
+    c.pin_bit_mask = BUTTON_PIN;
+    c.intr_type = GPIO_INTR_POSEDGE;
+    c.pull_down_en = c.pull_up_en = 0;
+    gpio_config(&c);
+
+    timer_button = xTimerCreate("TimerButton", pdMS_TO_TICKS(100), pdFALSE, 0, timer_buttons);
+    gpio_isr_handler_add(BUTTON_NUM, handle_button, NULL);
+    
+
+
+    // for(int a = 0;a < 9999999; a++) {
+        
+    // }
 
     // printf("SDK version:%s\n", esp_get_idf_version());
 
-    // //Initialize NVS
-    // esp_err_t ret = nvs_flash_init();
-    // if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
-    //   ESP_ERROR_CHECK(nvs_flash_erase());
-    //   ret = nvs_flash_init();
-    // }
-    // ESP_ERROR_CHECK(ret);
-        
-    // #if EXAMPLE_ESP_WIFI_MODE_AP
-    //     ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
-    //     wifi_init_softap();
-    // #else
-    //     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
-    //     wifi_init_sta();
-    // #endif /*EXAMPLE_ESP_WIFI_MODE_AP*/
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
+    wifi_init_softap();
 }
